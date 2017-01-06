27a7653353e73001ad18bf689e5b40a727130882
gabo05 <san.gaby05@gmail.com>
2017-01-06 03:20:31 -0600

    Change vars

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var ParseServer = require('parse-server').ParseServer;
 var app = express();
 var api = new ParseServer({
 	serverURL: 'https://goaltrainerps.herokuapp.com/parse',
-	databaseURI: databaseUri || 'mongodb://gtappuser:Gtdu0106@ds157268.mlab.com:57268/goaltrainer',
+	databaseURI: databaseUri || 'mongodb://localhost:27017/dev'
   	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-	appId: process.env.APP_ID || 'BEGT05013024',
-	fileKey: 'FKGT0501',
-	masterKey: process.env.MASTER_KEY || 'La2417_KmS30==DHM'/*,
+	appId: process.env.APP_ID || 'myAppId',
+	fileKey: 'myFileKey',
+	masterKey: process.env.MASTER_KEY || 'myMasterKey'/*,
 	push: {}, // See the Push wiki page
 	filesAdapter:{}*/
 });
@@ -18,5 +18,5 @@ app.use(mountPath, api);
 
 var port = 1337;
 app.listen(port, function() {
-	console.log('parse-server-example running on port ' + port + '.');
+	console.log('parse-server running on port ' + port + '.');
 });

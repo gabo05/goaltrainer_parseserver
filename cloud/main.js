244b6d29540b40a1faa56f39950de6e5b78e8b69
gabo05 <san.gaby05@gmail.com>
2017-01-12 20:19:59 -0600

    Confirm goals

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,32 @@
+Parse.Cloud.define('sendFriendRequest', function(req, res){
+	var query = new Parse.Query("User");
+	query.equalTo("objectId", req.params.user1);
+	query.first().then(function(user){
+		
+		query = new Parse.Query("User");
+		query.equalTo("objectId", req.params.user2);
+		query.first().then(function(friend){
+			query = new Parse.Query("Installation");
+			query.equalTo("user", friend);
+
+			Parse.Push.send({
+			 	where: query,
+			 	data: {
+			 		title: user.get("username")+" want to be your friend!",
+			 		message: "",
+			 		userId: user.get("objectId"),
+			 		type: 1,
+			 		goalId: ""
+			 	}
+			}, { useMasterKey: true })
+			.then(function() {
+			 	res.success({status: true});
+			}, function(error) {
+				res.success({status: false});
+			});
+		});
+	});
+});
 Parse.Cloud.define('makeFriends', function(req, res){
 	var query = new Parse.Query("User");
 	query.equalTo("objectId", req.params.user1);
@@ -14,7 +43,13 @@ Parse.Cloud.define('makeFriends', function(req, res){
 
 			Parse.Push.send({
 			 	where: query,
-			 	data: {title: user.get("username")+" accept your invitation!"}
+			 	data: {
+			 		title: user.get("username")+" accept your invitation!",
+			 		message: user.get("username")+" and you are friends now.",
+			 		type: 2,
+			 		userId: req.params.user2,
+			 		goaId: ""
+			 	}
 			}, { useMasterKey: true })
 			.then(function() {
 			 	res.success({status: true});
@@ -24,22 +59,59 @@ Parse.Cloud.define('makeFriends', function(req, res){
 		});
 	});
 });
-Parse.Cloud.define('sendFriendRequest', function(req, res){
+Parse.Cloud.define('sendConfirmRequest', function(req, res){
 	var query = new Parse.Query("User");
-	query.equalTo("objectId", req.params.user1);
+	query.equalTo("objectId", req.params.userid);
 	query.first().then(function(user){
-		
 		query = new Parse.Query("User");
-		query.equalTo("objectId", req.params.user2);
-		query.first().then(function(friend){
-			query = new Parse.Query("Installation");
-			query.equalTo("user", friend);
+		query.equalTo("friends", user);
+		query.find({
+			success: function(results){
+				query = new Parse.Query("Installation");
+				query.containedIn("user", results);
+				Parse.Push.send({
+				 	where: query,
+				 	data: {
+				 		title: user.get("username")+" want you to confirm a goal",
+				 		userId: user.get("objectId"),
+				 		message: req.params.goaldesc,
+				 		goalId: req.params.goalid,
+				 		type: 3
+				 	}
+				}, { useMasterKey: true })
+				.then(function() {
+				 	res.success({status: true});
+				}, function(error) {
+					res.success({status: false});
+				});
+			},
+			error: function(){
 
+			}
+		});
+	});
+});
+Parse.Cloud.define('confirmGoal', function(req, res){
+	var query = new Parse.Query("Goal");
+	query.equalTo("objectId", req.params.goalid);
+
+	query.first().then(function(goal){
+		goal.set("status", 3);
+		goal.increment("confirmed");
+		goal.save();
+		query = new Parse.Query("User");
+		query.equalTo("objectId", req.params.userid);
+		query.first().then(function(user){
+			query = new Parse.Query("Installation");
+			query.equalTo("userId", goal.get("userId"));
 			Parse.Push.send({
 			 	where: query,
 			 	data: {
-			 		title: user.get("username")+" want to be your friend!",
-			 		userId: user.get("objectId")
+			 		title: user.get("username")+" has confirmed your goal",
+			 		userId: user.get("objectId"),
+			 		message: goal.get("description"),
+			 		goalId: req.params.goalid,
+			 		type: 4
 			 	}
 			}, { useMasterKey: true })
 			.then(function() {
